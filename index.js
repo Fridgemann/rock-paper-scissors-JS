a3f8dbd15ef1b178896a2c189f55b8e023d2f72e
Fridgemann <120120633+Fridgemann@users.noreply.github.com>
2024-08-24 23:53:08 +0300

    Round functionality added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //Creating a const array which I can use to reference for the getHumanChoice function as well
 const moves = ["rock", "paper", "scissors"];
 
+//Creating two variables for the human and computer score
+let humanScore = 0;
+let computerScore = 0;
+
 //Create a function called getComputerChoice
 function getComputerChoice() {
     //Using Math.random() * moves.length, select a random element from the array (Remember to floor the walue since it's a floating point number)
@@ -10,7 +14,7 @@ function getComputerChoice() {
 }
 
 //For testing out if the randomization works properly
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 
 //Create a function called getHumanChoice
@@ -36,5 +40,36 @@ function getHumanChoice() {
     
 }
 
+//console.log(getHumanChoice());
 
-console.log(getHumanChoice());
+//Creating the function for round logic, two parameters which will be the human's and computer's moves
+function playRound(humanMove, computerMove) {
+    console.log(`You chose: ${humanMove}`);
+    console.log(`Computer chose: ${computerMove}`);
+    //Second index is for computer's move so set the outcome accordingly
+    //First index represents the user's choice, second index represents the correct result for the computer's move
+    let outcomes = {
+        "rock": {"rock": "Tie!", "paper": "You lose!", "scissors": "You win!"},
+        "paper": {"rock": "You win!", "paper": "Tie!", "scissors":"You lose!"},
+        "scissors": {"rock": "You lose!", "paper": "You win!", "scissors": "Tie!"}
+    }
+    //SWITCH statement to decide which score to increment
+    switch(outcomes[humanMove][computerMove]) {
+        case "You win!":
+            humanScore += 1;
+        case "You lose!":
+            computerScore += 1;
+    }
+    console.log(`computer score: ` ,computerScore);
+    console.log(`human score: ` ,humanScore);
+    return outcomes[humanMove][computerMove];
+}
+
+console.log(playRound(humanMove=getHumanChoice(), computerMove=getComputerChoice()))
+
+
+
+
+
+
+
